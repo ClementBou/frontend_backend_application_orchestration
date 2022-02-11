75e207aab96f2f2b94d0071635f00ac7af197784
Clement <clement.boulnager@efrei.net>
2022-02-11 14:29:05 +0100

    setting package json

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,6 +7,6 @@
  import axios from "axios";
 
  test("Access to backend", async() => {
-   const response = axios.get("http://0.0.0.0:3001/api")
+   const response = axios.get("http://localhost:3001/api")
    expect((await response).status).toBe(200);
  });
